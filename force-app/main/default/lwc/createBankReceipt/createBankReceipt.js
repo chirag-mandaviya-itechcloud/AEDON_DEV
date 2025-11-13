0459d77a5c92e3c0667a896df7cf6c2228071e1d
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2025-11-13 14:41:47 +0530

    add changes for record picker in currency and changes in css of inputs and add post button and get default currency from bank obj

--- force-app/main/default/lwc/createBankReceipt/createBankReceipt.js
+++ force-app/main/default/lwc/createBankReceipt/createBankReceipt.js
@@ -14,10 +14,10 @@ const columns = [
             target: '_self'
         }
     },
-    { label: 'Invoice Date', fieldName: 's2p3__Invoice_Date__c' },
-    { label: 'Customer Reference', fieldName: 's2p3__Customer_Reference__c' },
-    { label: 'Currency', fieldName: 'currencyName' },
-    { label: 'Gross Amount', fieldName: 's2p3__Gross_Amount__c' }
+    { label: 'Invoice Date', fieldName: 'formattedDate' },
+    { label: 'Posting Status', fieldName: 's2p3__Posting_Status__c' },
+    { label: 'Gross Amount', fieldName: 's2p3__Gross_Amount__c' },
+    { label: 'Currency', fieldName: 'currencyName' }
 ];
 export default class CreateBankReceipt extends LightningElement {
     @api recordId;
@@ -38,6 +38,7 @@ export default class CreateBankReceipt extends LightningElement {
     @track bankHeaderCreated = false;
     @track isModalLoading = false;
     @track bankReceiptId = '';
+    @track defaultCurrency = '';
 
     columns = columns;
 
@@ -53,7 +54,10 @@ export default class CreateBankReceipt extends LightningElement {
                 this.allBankReceiptData = result.map(row => ({
                     ...row,
                     recordLink: '/' + row.Id,
-                    currencyName: row.s2p3__Currency__r ? row.s2p3__Currency__r.Name : ''
+                    currencyName: row.s2p3__Currency__r ? row.s2p3__Currency__r.Name : '',
+                    formattedDate: new Date(row.s2p3__Invoice_Date__c)
+                        .toLocaleDateString('en-GB')
+                        .replace(/\//g, '/'),
                 }));
 
             }).catch(error => {
@@ -72,6 +76,8 @@ export default class CreateBankReceipt extends LightningElement {
                 }
                 if (result.bankAccountObj.s2p3__Currency__c != null && result.bankAccountObj.s2p3__Currency__c != undefined && result.bankAccountObj.s2p3__Currency__c != '') {
                     this.currencyId = result.bankAccountObj.s2p3__Currency__c;
+                    this.selectedCurrency = result.bankAccountObj.s2p3__Currency__c;
+                    this.defaultCurrency = result.bankAccountObj.s2p3__Currency__c;
                     this.createLookupCondition();
                 }
                 this.currencyISOCode = result.currencyISOCode;
@@ -95,11 +101,11 @@ export default class CreateBankReceipt extends LightningElement {
     }
 
     handleCurrencySelected(event) {
-        if (event.detail.length == 0) {
+        if (!event.detail.recordId) {
             this.selectedCurrency = null;
         }
-        if (event.detail.length > 0 && event.detail[0].id != undefined && event.detail[0].id != null) {
-            this.selectedCurrency = event.detail[0].id;
+        if (event.detail && event.detail.recordId != undefined && event.detail.recordId != null) {
+            this.selectedCurrency = event.detail.recordId;
         }
     }
 
@@ -157,7 +163,7 @@ export default class CreateBankReceipt extends LightningElement {
                 this.isLoading = false;
                 this.isModalLoading = false;
                 this.bankReceiptId = '';
-                // this.showToast('Error', 'Error Creating Bank Receipt: ' + error.body.message, 'error');
+                this.showToast('Error', 'Error Creating Bank Receipt: ' + error.body.message, 'error');
                 console.error('Error saving Record:', error);
             });
     }
@@ -166,41 +172,41 @@ export default class CreateBankReceipt extends LightningElement {
         this.accountSelected = '';
         this.selectedInvoiceDate = '';
         this.selectedReference = '';
-        this.selectedCurrency = '';
+        this.selectedCurrency = this.defaultCurrency;
         this.exchangeRate = '';
     }
 
     validateFields() {
         if (!this.accountSelected || this.accountSelected.trim() === '') {
-            // this.showToast('Warning', 'Please fill the Account.', 'warning');
+            this.showToast('Warning', 'Please fill the Account.', 'warning');
             return false;
         }
         if (!this.selectedInvoiceDate || this.selectedInvoiceDate.trim() === '') {
-            // this.showToast('Warning', 'Please fill the Invoice Date.', 'warning');
+            this.showToast('Warning', 'Please fill the Invoice Date.', 'warning');
             return false;
         }
         if (!this.selectedReference || this.selectedReference.trim() === '') {
-            // this.showToast('Warning', 'Please fill the Reference.', 'warning');
+            this.showToast('Warning', 'Please fill the Reference.', 'warning');
             return false;
         }
         if (!this.selectedCurrency || this.selectedCurrency.trim() === '') {
-            // this.showToast('Warning', 'Please fill the Currency.', 'warning');
+            this.showToast('Warning', 'Please fill the Currency.', 'warning');
             return false;
         }
         if (!this.exchangeRate || this.exchangeRate.trim() === '') {
-            // this.showToast('Warning', 'Please fill the Exchange Rate.', 'warning');
+            this.showToast('Warning', 'Please fill the Exchange Rate.', 'warning');
             return false;
         }
         return true;
     }
 
-    // showToast(mTitle, mMessage, mVariant) {
-    //     this.dispatchEvent(
-    //         new ShowToastEvent({
-    //             title: mTitle,
-    //             message: mMessage,
-    //             variant: mVariant
-    //         }),
-    //     )
-    // }
+    showToast(mTitle, mMessage, mVariant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: mTitle,
+                message: mMessage,
+                variant: mVariant
+            }),
+        )
+    }
 }
